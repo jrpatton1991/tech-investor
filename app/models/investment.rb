a34fe2211b236773b365ba37fdb7aeaf3884c60e
Passion McNabb <prmcnabb@gmail.com>
2016-05-20 15:22:52 -0500

    added associations and validations

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,8 @@
 class Investment < ActiveRecord::Base
+  validates :user_id, presence: true, numericality: true
+  validates :company_id, presence: true, numericality: true
+
+  belongs_to :user
+  belongs_to :company
+
 end
