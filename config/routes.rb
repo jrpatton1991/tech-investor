72615068849ddd5a81d8be903b10b2df1d9b852a
Passion McNabb <prmcnabb@gmail.com>
2016-05-23 10:38:53 -0500

    added companies controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :companies, only: [:show, :index]
-  get 'companies/index'
-
-  get 'companies/show'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
