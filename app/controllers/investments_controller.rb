e4e986f9568247d9bd01b1cc94c4fab72fa6ad51
Passion McNabb <prmcnabb@gmail.com>
2016-05-24 12:41:10 -0500

    added investments controller

---
+++ app/controllers/investments_controller.rb
@@ -0,0 +1,19 @@
+class InvestmentsController < ApplicationController
+
+  def create
+    # if create
+    #
+    # else
+    #
+    # end
+  end
+
+  def destroy
+    # if condition
+    #
+    # else
+    #
+    # end
+  end
+
+end
