f0777a3552dfe125a28b6a586bedadb3b7a258ad
Kayla Thomas <kaymthom@yahoo.com>
2016-05-24 19:30:09 -0500

    added investment link

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -2,8 +2,8 @@ class InvestmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-
-    
+    @investment = Investment.create(user_id: current_user.id, company_id: params[:company_id])
+    redirect_to root_path
   end
 
   def destroy
